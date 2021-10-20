d0b53060680f6d440cce2147ce6c8418899ad46e
Miles42 <aistra200050@gmail.com>
2021-10-20 11:58:10 +0200

    Added scripts of the Play, Stop, Pause buttons

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -22,24 +22,28 @@ void MainWindow::on_actionOpen_triggered()
 {
     QString filename = QFileDialog::getOpenFileName(this, "Open a File", "", "Video File(*.avi, *.mpg, *.mp4)");
     on_actionStop_triggered();
-    player->setSource(QUrl(filename));
+    player->setSource(QUrl::fromLocalFile(filename));
+    on_actionPlay_triggered();
 }
 
 
 void MainWindow::on_actionPlay_triggered()
 {
-
+    player->play();
+    ui->statusbar->showMessage("Playing");
 }
 
 
 void MainWindow::on_actionPause_triggered()
 {
-
+    player->pause();
+    ui->statusbar->showMessage("Paused...");
 }
 
 
 void MainWindow::on_actionStop_triggered()
 {
-
+    player->stop();
+    ui->statusbar->showMessage("Stopped");
 }
 
--- my-videoplayer.pro
+++ my-videoplayer.pro
@@ -1,4 +1,4 @@
-QT += core gui multimediawidgets multimedia
+QT += core gui multimedia multimediawidgets
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
